5991a7554c06efeb047e9e9b7d9c5bab1ac79f13
Pavel I. Kryukov <kryukov@frtk.ru>
2020-12-25 12:15:18 +0300

    Update sshhmac.c

--- sshhmac.c
+++ sshhmac.c
@@ -70,6 +70,7 @@ static void hmac_free(ssh2_mac *mac)
 
       ssh_hash_free(ctx->h_outer);
       ssh_hash_free(ctx->h_inner);
+      printf("%x\d", ctx->h_live);
       ssh_hash_free(ctx->h_live);
 //    smemclr(ctx->digest, ctx->hashalg->hlen);
 //    sfree(ctx->digest);
